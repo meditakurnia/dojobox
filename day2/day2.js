15eda5310996c052a311102feb2afb1b01af35da
meditakurnia <meditakurniaa@gmail.com>
2021-08-08 17:55:36 +0700

    test commit
    test commit

---
+++ day2/day2.js
@@ -0,0 +1,76 @@
+//alert('test');
+//for dan array tidak boleh dibuat variabel
+
+// var namaVariable = '12'; //camelcase
+// document.write(typeof(namaVariable) + '<br>');
+// namaVariabel = true;
+// document.write(typeof(namaVariable)+ '<br>');
+// namaVariabel = 123;
+// document.write(typeof(namaVariable)+ '<br>');
+
+// var namaPengunjung = prompt("Silahkan masukan nama kamu", "Meditaa" + '<br>')
+// document.write('Hai'+namaPengunjung + ',Selamat datang di Dojobox.ID' + '<br>');
+
+// var konfirmasi = confirm("Mau tetap disini kan ?");
+// var hasil = (konfirmasi==true)? "Iyadong" : "Gak deh..";
+// document.write(hasil);
+
+// var angka = parseInt(prompt("masukan angka yang mau ditest :"));
+// if(angka>10){
+//     document.write("bilangan yang dimasukan lebih dari 10");
+// }   else{
+//     document.write("bilangan yang dimasukan tidak terpenuhi");
+//     }
+
+// var nilai = prompt("masukan angka :");
+// if(nilai %2==0){
+//     document.write(nilai +' ' + 'merupakan bilangan genap');
+// } else if(nilai%2==1){
+//     document.write(nilai+ ' '+'merupakan bilangan ganjil');
+// }else{
+//     document.write(nilai + ' ' + 'bukan bilangan ganjil dan genap');
+// }
+
+// var nilaiKepuasan = prompt("Masukan nilai kepausan anda","0-5");
+
+// switch(nilaiKepuasan){
+//     case '0':
+//         document.write("Saya sangat kecewa");
+//         break
+//     case '1':
+//         document.write("Saya kecewa");
+//         break
+//     case '2':
+//         document.write('Saya tidak puas');
+//         break
+//     case '3' :
+//         document.write("Saya biasa aja");
+//         break
+//     case '4' :
+//         document.write("saya sangat puas");
+//         break
+//     case '5' :
+//         document.write("saya sanat sangat puas");
+//         default :
+//         document.write("masukan angkka yang sesuai")
+// }
+
+var nilaiKepuasan = parseInt(prompt("Masukan nilai kepausan anda","0-100"));
+
+switch(true){
+    case (nilaiKepuasan>=90):
+        document.write("Grade A");
+        break
+    case (nilaiKepuasan>=80):
+        document.write("Grade B");
+        break
+    case (nilaiKepuasan>=70):
+        document.write('Grade C');
+        break
+    case (nilaiKepuasan>=60) :
+        document.write("Grade D");
+        default :
+        document.write("Grade E")
+}
+
+
---
+++ day2/latihan.js
@@ -0,0 +1,48 @@
+// alert ('Selamat datang di dunia tipu...');
+// var nama = prompt("Masukan nama kamu :");
+// document.write('selamat datang kak ' + nama + '<br');
+
+// document.write('selamat datang kak putri' + '<br');
+// document.write('semangat belajar ' + '<br');
+// document.write("bilangan ketiga adalah " + bil3);
+
+
+// var bil1 = parseInt(prompt("Masukan bilangan pertama"));
+
+// var bil2 = parseInt(prompt("Masukan bilangan kedua"));
+
+// var bil3 = bil1 + bil2;
+// document.writeln("bilangan pertama adalah" +bil1);
+// document.writeln("bilangan kedua adalah " + bil2);
+// document.writeln("bilangan ketiga adalah " + parseInt(bil1 + bil2));
+
+// const angka1 = prompt("masukkan angka 1", "");
+// const angka2 = prompt ("masukkan angka 2", "");
+// var result = parseInt(angka1) + parseInt(angka2);
+// document.write("Hasilnya adalah " + result );
+
+// var nilai = 70;
+// var nilailulus = (nilai>75) ? 'lulus' : 'tidak lulus'
+// document.write(nilailulus);
+
+var a = 5;
+var b = 10;
+var c = a+b;
+document.write("Bilangan a = " +a);
+document.write('<br>')
+document.write("Bilangan b = " +b);
+document.write('<br>')
+document.write(a<b); //true
+document.write('<br>')
+document.write(b<a); //false
+document.write('<br>')
+document.write((c<a) && (b>a)); //false
+
+// var bil1 = 20;
+// var bil2 = 30;
+// var bil3 = bil1 + bil2;
+// document.write(bil1>bil2);
+// document.write('<br>');
+// document.write(bil1<bil2);
+// document.write('<br>');
+// document.write(!(bil1>bil2) && (bil1<bil2));
